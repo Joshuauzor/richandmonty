f60ba390009c0f14ef8a3d3318fd8e13a909224d
joshuauzor <joshuauzor10@gmail.com>
2023-09-21 00:14:20 +0200

    fix: filter logic

--- App.tsx
+++ App.tsx
@@ -1,10 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function App() {
+  const [input, setInput] = useState<string>('');
+  const [character, setCharacter] = useState<ICharacters | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<Array<ICharacters>>([]);
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    setLoading(true);
+    fetch('https://rickandmortyapi.com/api/character', {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        setData(responseData.results as ICharacters[]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        setLoading(false);
+      });
+  };
+
+  const handleSubmit = () => {
+    setCharacter(null);
+
+    for (let index = 0; index < data.length; index++) {
+      const element = data[index];
+      if (element.name.toLowerCase() === input.toLowerCase()) {
+        setCharacter(element);
+        break;
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Rick and Morty</Text>
+      <TextInput
+        value={input}
+        placeholder="Enter character name"
+        onChangeText={(input) => setInput(input)}
+        style={styles.inputField}
+        editable={data.length > 0}
+      />
+      {character && (
+        <Image
+          source={{
+            uri: character.image,
+          }}
+          style={styles.image}
+        />
+      )}
+
+      <TouchableOpacity
+        onPress={handleSubmit}
+        style={styles.button}
+        disabled={loading}
+      >
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +85,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputField: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '80%',
+    paddingHorizontal: 10,
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
   },
 });
---
+++ types/characters.tsx
@@ -0,0 +1,17 @@
+interface ICharacters {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string | null;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+}
